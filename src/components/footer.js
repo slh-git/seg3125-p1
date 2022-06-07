b182f6629c0fabac6a49f97c409f977ea7458757
Howard Tseng <howardt12345@gmail.com>
2022-06-06 20:59:00 -0400

    adjusted footer padding

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,15 +3,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const Footer = () => {
   return (
-    <footer class="pt-5 my-5 text-muted border-top text-center">
-      <a
-        href="https://github.com/howardt12345"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="text-decoration-none text-muted"
-      >
-        &copy; 2022 Howard Tseng
-      </a>
+    <footer class="my-4 text-muted border-top text-center">
+      <div class="pt-4">
+        <a
+          href="https://github.com/howardt12345"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="text-decoration-none text-muted"
+        >
+          &copy; 2022 Howard Tseng
+        </a>
+      </div>
     </footer>
   );
 };
