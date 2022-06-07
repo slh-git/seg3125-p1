cd384a983e900b8c4b04477aff6936f7de1e7083
Howard Tseng <howardt12345@gmail.com>
2022-06-07 13:41:10 -0400

    Footer in booking form

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,12 +6,12 @@ const Footer = () => {
     <footer class="my-4 text-muted border-top text-center">
       <div class="pt-4">
         <a
-          href="https://github.com/howardt12345"
+          href="https://github.com/howardt12345/seg3125-p1"
           target="_blank"
           rel="noopener noreferrer"
           class="text-decoration-none text-muted"
         >
-          &copy; 2022 Howard Tseng
+          &copy; {new Date().getFullYear()} Howard Tseng
         </a>
       </div>
     </footer>
